9a6eea4d0db033a27d12605e905b7fe4b63d18ce
aditiiprasad <aditi03prasad@gmail.com>
2024-12-30 11:18:43 +0530

    changes reversed due to some issues

--- frontend/src/components/HomePage.jsx
+++ frontend/src/components/HomePage.jsx
@@ -1,32 +1,21 @@
-import React, { useEffect, useState } from "react";
+import React from "react";
 import WardrobeUpload from "./WardrobeUpload";
 import WardrobeList from "./WardrobeList";
 import Taskbar from "./basics/Taskbar";
 import Footer from "./basics/Footer";
+
 import Topbar from "./Topbar";
 
 const HomePage = () => {
-  const [isLoggedIn, setIsLoggedIn] = useState(false);
   const email = localStorage.getItem("email"); 
-
-  useEffect(() => {
-    const token = localStorage.getItem("token");
-    if (token) {
-      setIsLoggedIn(true);
-    } else {
-      setIsLoggedIn(false);
-    }
-  }, []);
-
-  if (!isLoggedIn) {
-    return <div>Please log in to view this page.</div>; 
-  }
 
   return (
     <div className="bg-custom-blue min-w-max border-black border-2">
       <Taskbar />
 
-      <div className="mt-24">
+      <div className="mt-24  ">
+       
+     
         <Topbar/>
         <div id="wardrobeUpload">
           <WardrobeUpload />
